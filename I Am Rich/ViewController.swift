171f034159e5f86aac1596555f8830a621714bf3
Jason Ritchie <7eb94c5c@opayq.com>
2019-08-11 23:06:50 -0400

    Initial commit of completed app.

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jason Ritchie on 8/10/19.
+//  Copyright © 2019 Jason Ritchie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
